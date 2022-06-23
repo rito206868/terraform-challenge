2a708329f02dfbb2568871717c323039fed075d8
rito206868 <ritabrata.pablo@gmail.com>
2022-06-23 15:45:10 +0000

    updated

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "token" {
-type = string
-default = "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"
+    description = "Github access token"
+    default = "#################################"
 }
